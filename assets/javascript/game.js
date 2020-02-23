cd7117b8896fa5ec3362839f7d367395c35c3025
zibrahim2046 <zibrahim2046@gmail.com>
2020-02-22 20:58:53 -0600

    added crystals to html as well as styling to css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,15 +16,15 @@ $(document).ready(function() {
   // assign variables
 
   var targetNumber = targetNumber = Math.floor(Math.random() * 101 + 19);
-  //console.log(targetNumber);
+  console.log(targetNumber);
   var crystal1 = Math.floor(Math.random() * 11 + 1);
-  //console.log(crystal1);
+  console.log(crystal1);
   var crystal2 = Math.floor(Math.random() * 11 + 1);
-  //console.log(crystal2);
+  console.log(crystal2);
   var crystal3 = Math.floor(Math.random() * 11 + 1);
-  //console.log(crystal3);
+  console.log(crystal3);
   var crystal4 = Math.floor(Math.random() * 11 + 1);
-  //console.log(crystal4);
+  console.log(crystal4);
   var userTotal;
   var wins = 0;
   var losses = 0;
@@ -38,8 +38,8 @@ $(document).ready(function() {
     targetNumber = Math.floor(Math.random() * 101 + 19);
     
     userTotal = 0;
-    $("#total-score").text(userTotal);
-    $("#target-score").text(targetNumber);
+    $("#totalScore").text(userTotal);
+    $("#randomNumber").text(targetNumber);
   }
   // game start function
   function initialize() {
@@ -48,10 +48,10 @@ $(document).ready(function() {
     crystal3 = Math.floor(Math.random() * 11 + 1);
     crystal4 = Math.floor(Math.random() * 11 + 1);
     targetNumber = Math.floor(Math.random() * 101 + 19);
-    $("#target-score").text(targetNumber);
+    $("#randomNumber").text(targetNumber);
     $("#wins").text(wins);
     $("#losses").text(losses);
-    $("#total-score").text(userTotal);
+    $("#totalScore").text(userTotal);
   }
   // function if user wins or loses
   function winsLoss() {
@@ -61,5 +61,12 @@ $(document).ready(function() {
       wins++;
       $("#wins").text(wins);
     }
+    else if (userTotal > targetNumber) {
+        alert ("You Lose!");
+        reset();
+        losses++;
+
+    }
   }
+  initialize();
 });
